1afdbe6c5bf4b9ae2525662ac9a2c9aadd2abf02
Omar Morales <saul090594gmail.com>
2017-06-25 00:25:58 -0500

    laravel blog

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="jumbotron text-center">
+		<h1>{{ $title }}</h1>
+		<p>This is the laravel application from the "Laravel from Scratch Course"</p>	
+		<p>
+			<a href="/login" role="button" class="btn btn-primary btn-lg">Login</a>
+			<a href="/register" role="button" class="btn btn-success btn-lg">Register</a>
+		</p>
+	</div>
+@endsection
